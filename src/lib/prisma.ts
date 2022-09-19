84827e1b519fe76904aca1154016ab03b5cbe8b8
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2022-09-19 11:41:37 -0300

    Adiciona ajuste para deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import pkg, { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} 
+  const { PrismaClient: PrismaClientProd } = pkg
+  prisma = new PrismaClientProd()
+} else {
+  prisma = new PrismaClient()
+}
 
 export { prisma };
-
 
